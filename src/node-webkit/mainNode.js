71183a22f579e9784a9284a55118cb92a47b2c67
majodev <contact@majodev.com>
2014-11-07 18:22:56 +0100

    identation to tabs

--- src/node-webkit/mainNode.js
+++ src/node-webkit/mainNode.js
@@ -22,13 +22,13 @@ var homeDir = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFIL
 
 // write logData to log file in homeDirectory
 function logger(logData) {
-  var currDate = new Date() + ' - '
+	var currDate = new Date() + ' - '
 
-  fs.appendFile(path.join(homeDir, LOG_FILENAME), currDate + logData, function(err) {
-    if (err) {
-      console.error('Cannot write exception to log file' + err)
-    }
-  })
+	fs.appendFile(path.join(homeDir, LOG_FILENAME), currDate + logData, function(err) {
+		if (err) {
+			console.error('Cannot write exception to log file' + err)
+		}
+	})
 }
 
 // desktop notifications via node-notifier
@@ -36,50 +36,50 @@ function logger(logData) {
 // Object options { message, title, callback }
 function notify(options) {
 
-  if (typeof(options.callback) === 'function') {
-    openNotifyCallback = options.callback
-  } else {
-    openNotifyCallback = null
-  }
+	if (typeof(options.callback) === 'function') {
+		openNotifyCallback = options.callback
+	} else {
+		openNotifyCallback = null
+	}
 
-  notifier.notify({
-    title: (options.title === false || options.title.length === 0) ? pkg.name : options.title,
-    message: (options.message === false || options.message.length === 0) ? 'no message' : options.message, // empty string fails on mac!
-    icon: path.join(__dirname, pkg.window.icon),
-    contentImage: path.join(__dirname, pkg.window.icon),
-    sound: false, // Only Notification Center or Windows Toasters
-    wait: openNotifyCallback === null ? false : true, // wait with callback until user action is taken on notification
-    sender: 'com.intel.nw' // mac only, set sender (TODO, change id in plist) https://github.com/alloy/terminal-notifier#options
-  }, function(err, response) {
-    logger('error: ' + err + ' response: ' + response + '\n')
-    // response is response from notification
-  })
+	notifier.notify({
+		title: (options.title === false || options.title.length === 0) ? pkg.name : options.title,
+		message: (options.message === false || options.message.length === 0) ? 'no message' : options.message, // empty string fails on mac!
+		icon: path.join(__dirname, pkg.window.icon),
+		contentImage: path.join(__dirname, pkg.window.icon),
+		sound: false, // Only Notification Center or Windows Toasters
+		wait: openNotifyCallback === null ? false : true, // wait with callback until user action is taken on notification
+		sender: 'com.intel.nw' // mac only, set sender (TODO, change id in plist) https://github.com/alloy/terminal-notifier#options
+	}, function(err, response) {
+		logger('error: ' + err + ' response: ' + response + '\n')
+		// response is response from notification
+	})
 }
 
 
 
 notifier.on('click', function() {
-  // Happens if `wait: true` and user clicks notification
-  if (typeof(openNotifyCallback) === 'function') {
-    openNotifyCallback()
-    openNotifyCallback = null
-  }
+	// Happens if `wait: true` and user clicks notification
+	if (typeof(openNotifyCallback) === 'function') {
+		openNotifyCallback()
+		openNotifyCallback = null
+	}
 })
 
 notifier.on('timeout', function() {
-  // Happens if `wait: true` and notification closes
-  openNotifyCallback = null
+	// Happens if `wait: true` and notification closes
+	openNotifyCallback = null
 })
 
 // Catch all global exceptions for now by this listener
 // ATTENTION: this won't be the way to go with productive apps!
 // https://github.com/rogerwang/node-webkit/issues/1699
 process.on('uncaughtException', function(e) {
-  var logData = 'error: ' + e.stack + '\n'
+	var logData = 'error: ' + e.stack + '\n'
 
-  // log error to dev console
-  console.error(e)
-  logger(logData)
+	// log error to dev console
+	console.error(e)
+	logger(logData)
 })
 
 
